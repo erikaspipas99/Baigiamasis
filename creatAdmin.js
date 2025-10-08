323d797e100491f0fef169276e3f153c63ea3ff1
Erikas P <erikaspipas99@gmail.com>
2025-10-08 21:21:50 +0300

    created_user_login

---
+++ creatAdmin.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const JWT_SECRET = "password";
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const users = db.collection("user");
+  const user = await users.findOne({ username });
+
+  if (!user) return res.send("Bad Users");
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) return res.send("Bad password");
+
+  const token = jwt.sign(
+    { username: user.username, role: user.role },
+    JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  res.json({ token });
+});
