82897dff5e115aad4f49fdfca777c51599e8b0e7
Erikas P <erikaspipas99@gmail.com>
2025-10-13 19:54:56 +0300

    update_authRegion

--- authRegion.js
+++ authRegion.js
@@ -3,12 +3,12 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = "Region";
 
 export function authRegion(req, res) {
-  const authName = req.headers("autho");
-  const token = authName.split(" ");
+  const authName = req.headers["authorization"];
+  const token = authName && authName.split(" ");
 
   if (!token) return res.json("miss token");
-  jwt.verify(token, JWT_SECRET, (user) => {
-    if (!user) return res.json("Bad token");
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.json("Bad token");
     req.user = user;
   });
 }
